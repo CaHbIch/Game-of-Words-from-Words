2054f85accab549a1cdee2f399cf9883b89a72c4
CaHbI4 <FILlove@mail.ru>
2022-05-08 13:43:45 +0300

    ааааа

---
+++ classes.py
@@ -0,0 +1,64 @@
+import json
+
+
+class BasicWord:
+
+    def __init__(self, word, subwords):
+        self.word = word  # исходное слово
+        self.subwords = subwords  # набор допустимых подслов.
+        self.user_input = None
+
+    @classmethod
+    def stop_games(cls, user_input):
+        if user_input == 'стоп':
+            return user_input
+
+    def min_len(cls, user_input):
+        if len(user_input) < 3:
+            return user_input
+
+    def words(self):
+        """проверка введенного слова в списке допустимых подслов"""
+        if self.subwords == self.user_input:
+            return True
+
+    def count_subwords(self):
+        """подсчет количества подслов."""
+        return self.subwords
+
+    def __repr__(self):
+        return self.word
+
+
+class Player:
+
+    def __init__(self, name):
+        self.__name = name  # имя пользователя,
+        self.use_words = []  # использованные слова пользователя.
+
+    @classmethod
+    def len_name(cls, name):
+        if len(name) >= 3:
+            return name
+        else:
+            raise ValueError("Игровое имя меньше 3 букв ")
+
+    def word_count(self):
+        """получение количества слов"""
+        return len(self.use_words)
+
+    def add_used_word(self, user_input):
+        """добавление слова в использованные слова"""
+        if user_input in self.use_words:
+            return None
+        else:
+            self.use_words.append(user_input)
+
+    def is_word_unused(self, user_input):
+        """проверка использования данного слова до этого"""
+        if self.use_words:
+            return True
+        return False
+
+    def __repr__(self):
+        return self.__name
---
+++ main.py
@@ -0,0 +1,33 @@
+from classes import Player
+from function import load_random_word
+
+user_names = input("Ввведите имя игрока\n")
+
+player = Player(user_names)  # Экземпляр класса игрока
+
+if player.len_name(user_names):
+    print(f"Привет, {player}!")
+
+random_words = load_random_word()  # Переменая для функции получения рандомного слова.
+
+print(f"Составьте {len(random_words.count_subwords())} слов из слова - {random_words}")
+
+print(f"Слова должны быть не короче 3 букв\nПоехали, ваше первое слово?")
+
+for random_word in random_words.subwords:
+    user_input = input()
+    if user_input in random_words.subwords:
+        if not user_input in player.use_words:
+            player.add_used_word(user_input)
+            print("Верно 😇 ")
+        elif player.is_word_unused(user_input):
+            print("Слово уже есть 😈 ")
+    if random_words.stop_games(user_input):
+        print(f"Игра завершена!\nВы угадали {player.word_count()} слова")
+        break
+    if random_words.min_len(user_input):
+        print("Слова должны быть не короче 3 букв")
+    elif not user_input in player.use_words:
+        print("Нет допустимх слов 😛 ")
+else:
+    print(f"Cлова закончились игра завершена!\nВы угадали {player.word_count()} слов")
